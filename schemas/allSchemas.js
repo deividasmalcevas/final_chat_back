b150510b89846f7cc8edf4de34fe1a4850133bca
deividasmalcevas <deividasmalcevas@gmail.com>
2024-08-23 10:56:57 +0300

    first start

---
+++ schemas/allSchemas.js
@@ -0,0 +1,53 @@
+module.exports = {
+
+    user: {
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        avatar: {
+            type: String,
+            default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+        },
+        email: {
+            type: String,
+            required: false,
+        },
+        timeCreated: {
+            type: Number,
+            required: true,
+            default: Date.now()
+        },
+        timeUpdated: {
+            type: Number,
+            required: true,
+            default: Date.now()
+        },
+    },
+    errorDb: {
+        service: {
+            type: String,
+            required: true
+        },
+        file: {
+            type: String,
+            required: true
+        },
+        place: {
+            type: String,
+            required: true
+        },
+        error: {
+            type: String,
+            required: true,
+        },
+        time: {
+            type: String,
+            required: true,
+        }
+    }
+}
